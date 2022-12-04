3ce2df8fd7866765e2deb12cdf1d6a8ba6cef625
MarianaSembrak <117520714+MarianaSembrak@users.noreply.github.com>
2022-12-04 13:51:17 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+// Task_1
+
+let city = "Київ";
+city = "Львів";
+let address = city;
+alert(address);
+
+// Task_2
+
+let n = 5;
+let result = Math.pow(n, 3);
+console.log(result);
+
+// Task_3
+
+let pen = 4;
+let pensil = 6;
+let price_pen = 5.25;
+let price_p = 3.5;
+let total = pen * price_pen + pensil * price_p;
+document.write("Загальна вартість : ", total);
